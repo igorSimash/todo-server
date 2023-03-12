657844763584f34deb77e9ddb98f7c15ba6d3bb2
igorSimash <vigorochok@gmail.com>
2023-03-12 14:05:25 +0200

    samesite: none

--- index.ts
+++ index.ts
@@ -48,7 +48,7 @@ app.use(session({
 	cookie: {
 		maxAge: 1000 * 30 * 60 * 24 * 7 * 4, // 4 weeks
 		httpOnly: true,
-		sameSite: false,
+		sameSite: 'none',
 		secure: true,
 		// Domain: process.env.COOKIE_DOMAIN,
 	},
